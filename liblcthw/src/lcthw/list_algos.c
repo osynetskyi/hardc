695f45869d37eec08f0084b79551246d236273a4
osynetskyi <alexander.sinetskiy@gmail.com>
2015-01-01 16:09:25 +0200

    bubble sort fixed

--- liblcthw/src/lcthw/list_algos.c
+++ liblcthw/src/lcthw/list_algos.c
@@ -1,49 +1,33 @@
 #include <lcthw/list_algos.h>
-#include <lcthw/dbg.h>
 #include <assert.h>
 
 int List_bubble_sort(List *list, List_compare fn)
 {	
 	int swapped = 1;
+	ListNode *cur;
+	char *tmp; 
 	while(swapped == 1) {
 		swapped = 0;
-		LIST_FOREACH(list, first, next, cur) {
-			if((cur != list->last)&&(fn(cur->value, cur->next->value) > 0)) {
+		cur = list->first;
+		while(cur != list->last) {
+			if(fn(cur->value, cur->next->value) > 0) {
 				// swap 2 elemets
-				if(cur->prev == NULL) { 			// if we need to move first
-					list->first = cur->next;
-					cur->prev = list->first;
-					cur->next = list->first->next;
-					list->first->next->prev = cur;
-					list->first->prev = NULL;
-				} else if(cur->next == NULL) { 		// if we need to move last
-					cur->next->prev = cur->prev;
-					cur->next->next = cur;
-					cur->prev = cur->next;
-					cur->next = NULL;
-					list->last = cur;
-				} else { 							// other cases
-					cur->prev->next = cur->next;
-					cur->next->prev = cur->prev;
-					cur->next = cur->prev;
-					cur->prev = cur->prev->prev;
-					cur->next->next->prev = cur;
-					//cur->next->next = cur;
-				}
+				tmp = cur->value;
+				cur->value = cur->next->value;
+				cur->next->value = tmp;
 				swapped = 1;
 			}
+			cur = cur->next;
 		}
 	}
-
-	List_print(list, "sorted");
 	return 0;
 }
 
-/*List *merge(List *list1, List *list2)
+int List_merge(List *list1, List *list2, List *res)
 {
 	List_check(list1);
 	List_check(list2);
-	static List *res = List_create();
+	List_check(res);
 	
 	ListNode *a = list1->first;
 	ListNode *b = list2->first;
@@ -58,7 +42,7 @@ int List_bubble_sort(List *list, List_compare fn)
 		}
 	}
 
-	if((a == NULL) && (b != NULL)) {
+	/*if((a == NULL) && (b != NULL)) {
 		LIST_FOREACH(list2, b, next, cur) {
 			List_push(res, cur->value);
 		}
@@ -66,18 +50,10 @@ int List_bubble_sort(List *list, List_compare fn)
 		LIST_FOREACH(list1, a, next, cur) {
 			List_push(res, cur->value);
 		}
-	}
-	
-	return res;
-}*/
-
-/*int List_halfsplit(List *list1, List *list2)
-{
-	int n = List_count(list1);
-	int middle = n / 2;
+	}*/
 	
 	return 0;
-}*/
+}
 
 int List_merge_sort(List *list, List_compare fn)
 {
--- liblcthw/src/lcthw/list_algos.h
+++ liblcthw/src/lcthw/list_algos.h
@@ -2,6 +2,7 @@
 #define lcthw_List_algos_h
 
 #include <lcthw/list.h>
+#include <lcthw/dbg.h>
 #include <stdlib.h>
 
 int List_bubble_sort(List *list, List_compare fn);
